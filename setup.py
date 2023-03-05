58f9e0745a7b6a51c014722f6688356c11e66c49
jmhasan1 <jmhasan17@gmail.com>
2023-03-05 23:21:23 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+#The setup script is the centre of all activity in building, distributing, and installing modules using the Distutils.
+# will be responsible for creating my entire ML application as  a package
+from setuptools import find_packages , setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    """
+    this function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+setup(
+    name = "mlProject",
+    version = '0.0.1',
+    author='Jahid Hasan',
+    author_email='itsmejahidhasan@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt'),
+
+
+)
